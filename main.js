f33307c2263fd9f216686d85167acaafd238aa6b
ClearCrystalSkies <100354673+ClearCrystalSkies@users.noreply.github.com>
2023-04-04 16:48:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+img = "";
+status = "";
+objects = [];
+function preload(){
+    img = loadImage("dog_cat.jpg");
+}
+function setup(){
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+}
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML= "Status: Detecting object.."
+}
+function draw(){
+    image(video, 0, 0, 380, 380);
+    if (status != "") {
+        for (let i =0;  i<objects.length; i++) {
+            r = random(255);
+            g = random(255);
+            b = random(255);
+            document.getElementById("status").innerHTML= "Status : Object Detected";
+            document.getElementById("no_of_objects").innerHTML = "Number of objects detected : " + objects.length ;
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label+ " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+function modelLoaded(){
+    console.log("Model has loaded!");
+    status = true;
+    objectDetector.detect(video, gotResults);
+}
+function gotResults(error, results){
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(results);
+        objects = results;
+    }
+}
